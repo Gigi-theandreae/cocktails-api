606e7d3fcc83e24dccc253fbd8f1d8ecba90c32c
Edward Bernal <accounts@eddiebernal.com>
2022-09-05 13:36:34 -0700

    fixed up

--- js/main.js
+++ js/main.js
@@ -4,74 +4,75 @@ function getDrink() {
   const choice = document.querySelector("input").value;
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`;
 
+  
+  function selectedItem(e, drinks) {
+
+    let drinkID = e.target.dataset.drinkid; // which link what clicked on?  whats the drink ID
+
+    // Removed any and all selected drinks
+    let toRemove = document.getElementById("selected_drink");
+
+      while (toRemove.firstChild) {
+        toRemove.removeChild(toRemove.firstChild);
+     }
+     //
+   
+     //loop through all drinks
+    drinks.forEach((drink) => {
+
+    
+      // find the matching drink by ID
+       if(drink.idDrink == drinkID){
+
+        //if theres a match lets create the HTML object
+          let li = document.createElement("li");
+
+          let h1 = document.createElement("h1");
+          h1.textContent = drink.strDrink;
+          li.appendChild(h1);
+      
+          let img = document.createElement("img");
+          img.src = drink.strDrinkThumb;
+          li.appendChild(img);
+      
+          let p = document.createElement("p");
+          p.textContent = drink.strInstructions;
+          li.appendChild(p);
+
+          document.getElementById("selected_drink").appendChild(li);
+    
+      }
+
+    });
+
+  }
+   // EOF List
+
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log("ALL DRINKS", data.drinks);
+     // console.log("ALL DRINKS", data.drinks);
 
+     let toRemove = document.getElementById("drinks");
+
+     while (toRemove.firstChild) {
+      toRemove.removeChild(toRemove.firstChild);
+    }
+ 
       data.drinks.forEach((obj) => {
         /// console.log(obj.strDrink + obj.strDrinkThumb + obj.strInstructions);
-
-        //  <li><a>
-        //   <h1>{NAME}</h1>
-        //   <img></img>
-        //   <p></p>
-        //  </a></li>
-
-        // Insert hidden element
-        let li = document.createElement("li");
-        li.setAttribute("id", obj.idDrink);
-        
-
-
         //Lets make a list
-        // li = document.createElement("li"); 
 
+        let li = document.createElement("li");
         let a = document.createElement("a");
-        a.setAttribute("data-src", obj.idDrink);
+        a.setAttribute("data-drinkid", obj.idDrink); // This
         a.textContent = obj.strDrink;
         li.appendChild(a);
-
+        a.addEventListener("click", (e) => selectedItem(e, data.drinks)); // This
         document.getElementById("drinks").appendChild(li);
 
-        
-        
-
-        document.querySelector('#drinks').addEventListener ('click', selectedItem)
-
-        function selectedItem(){
-
-
-          document.getElementById("selected_drink").appendChild(li)
-
-          
-
-          // how to just show the selected drink and hide the others.
-          // this shows all the options with the name,img and inst.
-          for (let i = 0; i < 30; i++) {
-          
-          let h1 = document.createElement("h1");
-          h1.textContent = `obj.strDrink${i}`;
-          li.appendChild(h1);
-
-          let img = document.createElement("img");
-          img.src = obj.strDrinkThumb;
-          li.appendChild(img);
-
-          let p = document.createElement("p");
-          p.textContent = obj.strInstructions;
-          li.appendChild(p);
-
-
-          document.querySelector('a').style.display = "none"
-          }
-        }
-
-        
       });
     })
-
-
     .catch((err) => {
       console.log(`error ${err}`);
     });
