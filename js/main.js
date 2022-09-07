0c974b17f1f5cd9cbdae08f1da2b17d49d39997e
Gigi-theandreae <gzmgezgin@gmail.com>
2022-09-07 11:34:07 +0200

    clear list function added

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ function getDrink() {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`;
 
   
+
   function selectedItem(e, drinks) {
 
     let drinkID = e.target.dataset.drinkid; // which link what clicked on?  whats the drink ID
 
+    
     // Removed any and all selected drinks
     let toRemove = document.getElementById("selected_drink");
 
@@ -16,6 +18,8 @@ function getDrink() {
         toRemove.removeChild(toRemove.firstChild);
      }
      //
+
+
    
      //loop through all drinks
     drinks.forEach((drink) => {
@@ -43,6 +47,13 @@ function getDrink() {
     
       }
 
+
+      document.querySelector("button").addEventListener("click", clearList);
+
+      function clearList(){
+        document.querySelector('ul').innerHTML = '';
+      }
+
     });
 
   }
@@ -60,9 +71,8 @@ function getDrink() {
     }
  
       data.drinks.forEach((obj) => {
-        /// console.log(obj.strDrink + obj.strDrinkThumb + obj.strInstructions);
+        
         //Lets make a list
-
         let li = document.createElement("li");
         let a = document.createElement("a");
         a.setAttribute("data-drinkid", obj.idDrink); // This
@@ -80,12 +90,6 @@ function getDrink() {
 
 
 
-/// ----- select all li from ul  display none
-/// select the li with the id that I chose and show that one
-// clear the previous search when enter new input (choice.clear();?)
-
-
-// add an event listener to idDrink to display selected drink, and hide the other options
 
       
   
